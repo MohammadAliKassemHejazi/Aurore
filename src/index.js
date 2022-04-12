358866feb480379aa5068df502e7227783ccd1a0
Mohammad Ali Kassm Hejazi <mohammad_hejazii@outlook.com>
2022-04-12 14:33:20 +0300

    build f

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -5,10 +7,10 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
